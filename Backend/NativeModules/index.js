a0a6226ded2a3f1bc80889c11aae72aa7b115a08
Sonal Mittal <sonalmittal140103@gmail.com>
2024-09-19 21:48:18 +0530

    Native Modules

--- Backend/NativeModules/index.js
+++ Backend/NativeModules/index.js
@@ -1,2 +1,20 @@
 const fs = require("fs");
 
+// fs.writeFile("message.txt","Hello from Node!",(err) => {
+//     if (err) throw err;
+//     console.log("The file has been saved");
+// });
+
+// fs.readFile("./message.txt","utf8",(err,data) =>{
+//     if (err) throw err;
+//     console.log("Error h bhai...Code sahi kar le");
+    
+// })
+
+fs.readFile("./message.txt", "utf8", (err, data) => {
+    if (err) {
+        console.error("Error h bhai...Code sahi kar le", err.message);
+        return;  // Exit the function if there's an error
+    }
+    console.log("File content:", data);  // Display the file content
+});
